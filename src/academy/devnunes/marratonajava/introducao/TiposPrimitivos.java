0448a7b39709e4c48227a9ad21eeb3b6eeee5a97
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-07 22:27:52 -0300

    Annotation and modification in the primitive types folder
    Fiz uma pequena alteração e fiz um comentário na linha 9 para eu lembra para que serve a funcionalidade daquela linha e no settings so estava testando outros niveis de versões do Java

--- src/academy/devnunes/marratonajava/introducao/TiposPrimitivos.java
+++ src/academy/devnunes/marratonajava/introducao/TiposPrimitivos.java
@@ -6,14 +6,20 @@ public class TiposPrimitivos {
         int age = 10; //4 bytes 
         long numberLarge = 200000; //8 bytes
         double salaryDouble = 2000; // 8bytes
-        float salaryFloat = 2500.0F; //4 bytes
+        float salaryFloat = (float) 2500.0D; // forçando o tipo da variavel para ela mesma (4 bytes)
         byte ageByte = 20; // 1 byte 
-        short ageShort = 10; // 2bytes
+        short ageShort = 65; // 2bytes
         boolean correct = true; // 1 bit
         boolean incorrect = false; // 1 bit
-        char caractere = 'M'; // 2 bytes (tabela ascii e Unicode)
+        char caractere = 56; // 2 bytes (tabela ascii e Unicode)
+        // Strings
+        String name = " Cayque";
+        var name2 = "Nunes ";
         System.out.println(age);
         System.out.println(correct);
-
+        System.out.println(incorrect);
+        System.out.println("char "+ caractere);
+        System.out.println("Hi my name is" + name);
+ 
     }
 }   
