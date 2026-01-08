66b0449ec86ef0421e5515e066a4d63c3b62c15e
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-07 22:30:39 -0300

    A short exercise to manipulate some primitive data types and concatenate the manipulated data.
    um pequeno exercicios para manipular alguns tipos primitivos e fazer a concanetação dos dados manipulados, um exercicios básico mas essencial para o começo

---
+++ src/academy/devnunes/marratonajava/introducao/TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package academy.devnunes.marratonajava.introducao;
+
+public class TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+        String name = "Cayque";
+        String address = "Rua de fulano de tal, 134";
+        double salary = 2450.32;
+        String dateOfReceipt = "05/01/2026";
+        String report = "Eu " + name + ", morador do endereço " + address + ", declaro que recebi o salário de R$ " + salary + " na data " + dateOfReceipt + ".";
+        System.out.println(report);
+    }
+
+}
