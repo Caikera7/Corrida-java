e5297a683a4d4344afb831d1304cd282c936d0b1
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-09 22:01:31 -0300

    Continuing the learning of logical and relational conditions
    Continuação do aprendizado do estudo de códigos lógicos e relacionais e também coloquei um aprendizado de incremento e decremento

--- src/academy/devnunes/marratonajava/introducao/TiposPrimitivos.java
+++ src/academy/devnunes/marratonajava/introducao/TiposPrimitivos.java
@@ -12,7 +12,8 @@ public class TiposPrimitivos {
         boolean correct = true; // 1 bit
         boolean incorrect = false; // 1 bit
         char caractere = 56; // 2 bytes (tabela ascii e Unicode)
-        // Strings
+
+        // Strings classe
         String name = " Cayque";
         var name2 = "Nunes ";
         System.out.println(age);
@@ -20,6 +21,53 @@ public class TiposPrimitivos {
         System.out.println(incorrect);
         System.out.println("char "+ caractere);
         System.out.println("Hi my name is" + name);
- 
+
+        //Relacionais 
+        boolean isAdult = age >= 18;
+        System.out.println("É adulto? " + isAdult);
+        boolean isMinor = age < 18;
+        System.out.println("É menor de idade? " + isMinor);
+        boolean canVote = isAdult || age == 16;
+        System.out.println("Pode votar? " + canVote);
+        boolean canNotVote != canVote;
+        System.out.println("Não pode votar? " + canNotVote);
+
+
+        //Logicos 
+        int age = 30;
+        float salary = 3500F;
+        boolean isWithinTheLawToBeOverThirty = age > 30 && salary >= 4612;
+        System.out.println("Is within the law to be over thirty? " + isWithinTheLawToBeOverThirty);
+        boolean isWithinTheLawToBeUnderThirty = age < 30 && salary >= 3381;
+        System.out.println("Is within the law to be under thirty? " + isWithinThe   LawToBeUnderThirty);
+
+        double currentAccount = 300;0;
+        double savingsAccount = 10000;
+        float valuePlayStation = 5000F;
+
+        boolean isPlayStationPurchasable = currentAccount > valuePlayStation || savingsAccount > valuePlayStation;
+        System.out.println("Is PlayStation purchasable? " + isPlayStationPurchasable);
+
+        //Atribuição
+        double bonus = 1800;
+        bonus += 1000; // bonus = bonus + 1000
+        System.out.println(bonus);
+        bonus -= 1000; // bonus = bonus - 1000
+        System.out.println(bonus);
+        bonus *= 2; // bonus = bonus * 2    
+        System.out.println(bonus);
+        bonus /= 2; // bonus = bonus / 2
+        System.out.println(bonus);
+        bonus %= 2; // bonus = bonus % 2
+        System.out.println(bonus);
+
+        //Incrementos e Decrementos
+        int increment = 0;
+        increment++; 
+        System.out.println(increment);
+        increment--;
+        System.out.println(increment);
+
+        //Quando o incremento estiver na frente o valor é somado antes de ser usado & quando estiver atrás o valor é somado depois de ser usado (increment++ / ++increment)
     }
 }   
