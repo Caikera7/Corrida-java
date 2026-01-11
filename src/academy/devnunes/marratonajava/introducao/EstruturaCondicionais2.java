914092c52cc85c562c54bdd18060c5fc4e1b2d5b
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-10 22:41:53 -0300

    Learning about ternary rhythms; I will soon be doing more practical exercises.

---
+++ src/academy/devnunes/marratonajava/introducao/EstruturaCondicionais2.java
@@ -0,0 +1,13 @@
+package academy.devnunes.marratonajava.introducao;
+
+public class EstruturaCondicionais2{
+    
+    public static void main(String[] args) {
+     
+        //ternary operator
+        double salaryMen = 3000;
+        String mensage = salaryMen > 3000 ? "Eu vou realizar o pagamento" : "Não vou realizar o pagamento";
+        System.out.println(mensage);
+        // (condição) ? verdadeiro : falso
+    }
+}
