17579ad8cfe685eb79ed98daa37d021c9ab56a7c
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-11 19:30:57 -0300

    Creating a learning folder with another conditional structure: Switch.

---
+++ src/academy/devnunes/marratonajava/introducao/EstruturaCondicionais3.java
@@ -0,0 +1,47 @@
+package academy.devnunes.marratonajava.introducao;
+
+public class EstruturaCondicionais3 {
+    public static void main(String[] args) {
+        //switch case
+        byte day = 1;
+        switch (day){
+            case 1:
+                System.out.println("Segunda-feira");
+                break;
+            case 2:
+                System.out.println("Terça-feira");
+                break;
+            case 3:
+                System.out.println("Quarta-feira");
+                break;
+            case 4:
+                System.out.println("Quinta-feira");
+                break;
+            case 5:
+                System.out.println("Sexta-feira");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Dia inválido");
+                break;
+        }
+
+        char sexo = 'H';
+        switch (sexo){
+            case 'H':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Sexo inválido");
+                break;
+        }
+    }
+}
