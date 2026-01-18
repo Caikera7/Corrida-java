bfe321b6d0dc5cf4f92de170f8a45b5ef4de6901
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-17 22:02:10 -0300

    Criação do arquivo de Arrays multidimencionais

---
+++ src/academy/devnunes/marratonajava/introducao/ArraysMultidimencionais.java
@@ -0,0 +1,19 @@
+package academy.devnunes.marratonajava.introducao;
+
+public class ArraysMultidimencionais {
+    public static void main (String[] args){
+        //Um arrays dentro de outro Array
+        int[][] day = new int[3][3];
+        day[0][0] = 31; 
+        day[0][1] = 28; 
+        day[0][2] = 31; 
+
+        day[1][0] = 31; 
+        day[1][1] = 30; 
+        day[1][2] = 31;
+        
+        
+
+        for(int i = 0; i <day.len)
+    }
+}
