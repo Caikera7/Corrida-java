dba2cd9b239628b2a7f4f232045c73c048a00e3f
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-18 21:01:53 -0300

    continuation of learning about multidimensional arrays
    Having provided some examples and practices for initializing and understanding array syntax, I've done just that.

--- src/academy/devnunes/marratonajava/introducao/ArraysMultidimencionais.java
+++ src/academy/devnunes/marratonajava/introducao/ArraysMultidimencionais.java
@@ -6,14 +6,26 @@ public class ArraysMultidimencionais {
         int[][] day = new int[3][3];
         day[0][0] = 31; 
         day[0][1] = 28; 
-        day[0][2] = 31; 
+        day[0][2] = 20; 
+        System.out.println(day[0][0]);
 
-        day[1][0] = 31; 
+        day[1][0] = 15; 
         day[1][1] = 30; 
-        day[1][2] = 31;
+        day[1][2] = 28;
         
-        
+        for(int i = 0; i < day.length; i++){
+            for(int j = 0; j <day[i].length; j++){
+                System.out.println(day[i][j]);
+            }
+        }
 
-        for(int i = 0; i <day.len)
+        //Usando o foreach no Arrays Multidimencionais
+        System.out.println("----------------------------");
+
+        for(int[] arr: day   ){
+            for(int num:arr) {
+                System.out.println(num);
+            }
+        }
     }
 }
---
+++ src/academy/devnunes/marratonajava/introducao/ArraysMultidimencionais2.java
@@ -0,0 +1,18 @@
+package academy.devnunes.marratonajava.introducao;
+
+public class ArraysMultidimencionais2 {
+    public static void main(String[] args) {
+        int[][] arrInt = new int[3][];
+
+        //Inicialização de Array Multidimencionais
+        arrInt[0] = new int[2];
+        arrInt[1] = new int[3];
+        arrInt[2] = new int[4];
+
+        for(int[] arrBase: arrInt){
+            for(int num: arrBase){
+                System.out.println(num);
+            }
+        }
+    }
+}
