7274455fd1a4553fd31da57bf0dc58b02b99be8b
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-01-22 01:01:39 -0300

    folder of method objects

---
+++ src/academy/devnunes/marratonajava/javacore/introducaometodos/domain/Calculadora.java
@@ -0,0 +1,13 @@
+package academy.devnunes.marratonajava.javacore.introducaometodos.domain;
+
+public class Calculadora {
+    
+    //sem retorno
+    public void sum(){
+        System.out.println(10 + 10);
+    }
+
+    public void subtraction(){
+        System.out.println(34-3);
+    }
+}
---
+++ src/academy/devnunes/marratonajava/javacore/introducaometodos/test/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package academy.devnunes.marratonajava.javacore.introducaometodos.test;
+
+import academy.devnunes.marratonajava.javacore.introducaometodos.domain.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calculator = new Calculadora();
+        calculator.sum();
+        calculator.subtraction();
+    }
+}
