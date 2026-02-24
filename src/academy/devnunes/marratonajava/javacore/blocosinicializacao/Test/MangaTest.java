a90d61568a44960fb8aafcaa6c621e25cf60dd50
Cayque Nunes <cayquenuneslealmarquescordeiro@gmail.com>
2026-02-23 22:02:03 -0300

    Bloco de inicialização de instância
    Blocos de inicialização de instância serve para quando um objeto for criado fazer verificações ou validações com aquele objeto criado, referindo para todos os bloco inicializazdos.

---
+++ src/academy/devnunes/marratonajava/javacore/blocosinicializacao/Test/MangaTest.java
@@ -0,0 +1,11 @@
+package academy.devnunes.marratonajava.javacore.blocosinicializacao.Test;
+
+import academy.devnunes.marratonajava.javacore.blocosinicializacao.domain.Manga;
+
+public class MangaTest {
+    public static void main(String[] args) {
+        Manga manga = new Manga("Cayque");
+
+        
+    }
+}
---
+++ src/academy/devnunes/marratonajava/javacore/blocosinicializacao/domain/Manga.java
@@ -0,0 +1,19 @@
+package academy.devnunes.marratonajava.javacore.blocosinicializacao.domain;
+
+public class Manga {
+    private String name;
+    private int[] episodios = {1,2,3,4,5,6,7,8,9,10};
+
+    //bloco de Iniciaçização de criação do objeto 
+    {
+        for(int ep: this.episodios){
+            System.out.println(ep + " ");
+        }
+
+    }
+
+    public Manga(String name){
+        this.name = name;
+    }
+  
+}
